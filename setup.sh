c2d213d6e3ddf35ddffb9549b02660a7657d7efe
a-szk <atsushisuzuki2k@gmail.com>
2023-01-22 09:39:59 +0900

    add setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+self_dir=$(cd $(dirname $0); pwd)
+cd $self_dir/..
+
+mkdir -p .config/nvim
+mkdir -p .config/compton
+mkdir -p .config/i3
+mkdir -p .config/i3statu
+ln -sf $self_dir/_vimrc .vimrc
+ln -sf $self_dir/_vim .vim
+ln -sf $self_dir/_vim .nvim
+ln -sf $self_dir/compton.conf .config/compton/compton.conf
+ln -sf $self_dir/_vimrc .config/nvim/init.vim
+ln -sf $self_dir/i3-config .config/i3/config
+ln -sf $self_dir/i3status.conf .config/i3status/i3status.conf
+
+sudo apt install vim neovim compton i3
+
