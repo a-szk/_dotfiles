fc91aa42fb80f60431cc6cdd9944b8f307673caa
a-szk <atsushisuzuki2k@gmail.com>
2023-01-22 15:43:44 +0900

    remove vim plugins

--- setup.sh
+++ setup.sh
@@ -6,14 +6,15 @@ cd $self_dir/..
 mkdir -p .config/nvim
 mkdir -p .config/compton
 mkdir -p .config/i3
-mkdir -p .config/i3statu
+mkdir -p .config/i3status
+ln -sf $self_dir/_bashrc .bashrc
 ln -sf $self_dir/_vimrc .vimrc
 ln -sf $self_dir/_vim .vim
 ln -sf $self_dir/_vim .nvim
 ln -sf $self_dir/compton.conf .config/compton/compton.conf
 ln -sf $self_dir/_vimrc .config/nvim/init.vim
 ln -sf $self_dir/i3-config .config/i3/config
-ln -sf $self_dir/i3status.conf .config/i3status/i3status.conf
+ln -sf $self_dir/i3status.conf .config/i3status/config
 
-sudo apt install vim neovim compton i3
+sudo apt install vim neovim compton i3 xsel tree colordiff feh
 
