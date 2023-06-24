b85bff7d8d74b9053fadc3f91335c8d9be454229
a-szk <atsushisuzuki2k@gmail.com>
2023-06-24 23:44:41 +0900

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -3,18 +3,22 @@
 self_dir=$(cd $(dirname $0); pwd)
 cd $self_dir/..
 
+sudo apt -y install git
+
 mkdir -p .config/nvim
 mkdir -p .config/compton
 mkdir -p .config/i3
 mkdir -p .config/i3status
+mkdir -p .vim/bundle
+mkdir -p .nvim/bundle
 ln -sf $self_dir/_bashrc .bashrc
 ln -sf $self_dir/_vimrc .vimrc
-ln -sf $self_dir/_vim .vim
-ln -sf $self_dir/_vim .nvim
 ln -sf $self_dir/compton.conf .config/compton/compton.conf
 ln -sf $self_dir/_vimrc .config/nvim/init.vim
 ln -sf $self_dir/i3-config .config/i3/config
 ln -sf $self_dir/i3status.conf .config/i3status/config
+git clone https://github.com/Shougo/neobundle.vim.git .vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/neobundle.vim.git .nvim/bundle/neobundle.vim
 
-sudo apt install vim neovim compton i3 xsel tree colordiff feh
+sudo apt -y install vim neovim compton i3 xsel tree colordiff feh
 
