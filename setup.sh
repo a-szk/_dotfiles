adf595a325150b71d2c4030cf07a6d632adaf60e
a-szk <atsushisuzuki2k@gmail.com>
2023-08-24 09:00:28 +0900

    setup.sh : modify to link _bin/ files

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ mkdir -p .config/compton
 mkdir -p .config/i3
 mkdir -p .config/i3status
 mkdir -p .nvim/bundle
+mkdir -p bin
 
 if [[ ! e .nvim/bundle/neobundle.vim]]; then
     git clone https://github.com/Shougo/neobundle.vim.git .nvim/bundle/neobundle.vim
@@ -22,10 +23,10 @@ ln -sf $self_dir/_bashrc .bashrc
 ln -sf $self_dir/_tmux.conf .tmux.conf
 ln -sf $self_dir/compton.conf .config/compton/compton.conf
 ln -sf $self_dir/_vimrc .config/nvim/init.vim
-ln -sf $self_dir/_bin bin
 ln -sf $self_dir/_byobu .byobu
 ln -sf $self_dir/i3-config .config/i3/config
 ln -sf $self_dir/i3status.conf .config/i3status/config
+ls $self_dir/_bin | xargs -n1 -IXXX ln -sf $self_dir/_bin/XXX bin/XXX
 
 sudo apt -y install neovim xsel tree colordiff compton feh vlc ffmpeg byobu x11vnc i3* ttf-dejavu* fonts-ipafont pm-utils net-tools indicator-cpufreq python-gi python-gi-cairo python3-gi python3-gi-cairo gir1.2-gtk-3.0 exfat-fuse exfat-utils openssh-server fcitx-mozc clang-format-12
 
