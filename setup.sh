29b5e8258d1c8de22c50c20ca4e04a5f88632996
a-szk <atsushisuzuki2k@gmail.com>
2023-08-24 09:50:31 +0900

    remove i3wm

--- setup.sh
+++ setup.sh
@@ -8,8 +8,6 @@ cd $HOME
 
 mkdir -p .config/nvim
 mkdir -p .config/compton
-mkdir -p .config/i3
-mkdir -p .config/i3status
 mkdir -p .nvim/bundle
 mkdir -p bin
 
@@ -20,12 +18,10 @@ ln -sf $self_dir/_vimrc .config/nvim/init.vim
 rm -rf .byobu
 ln -sf $self_dir/_byobu .byobu
 ln -sf $self_dir/_tmux.conf .tmux.conf
-ln -sf $self_dir/_i3-config .config/i3/config
-ln -sf $self_dir/_i3status.conf .config/i3status/config
 ls $self_dir/_bin | xargs -n1 -IXXX ln -sf $self_dir/_bin/XXX bin/XXX
 
 sudo apt update
-sudo apt -y install git neovim xsel tree colordiff compton feh vlc ffmpeg byobu x11vnc i3* ttf-dejavu* fonts-ipafont pm-utils net-tools indicator-cpufreq python-gi python-gi-cairo python3-gi python3-gi-cairo gir1.2-gtk-3.0 exfat-fuse exfat-utils openssh-server fcitx-mozc clang-format-12
+sudo apt -y install git neovim xsel tree colordiff compton feh vlc ffmpeg byobu x11vnc pm-utils net-tools indicator-cpufreq python-gi python-gi-cairo python3-gi python3-gi-cairo gir1.2-gtk-3.0 exfat-fuse exfat-utils openssh-server fcitx-mozc clang-format-12
 
 # nvim
 if [ ! -e .nvim/bundle/neobundle.vim ]; then
@@ -42,6 +38,3 @@ sudo mkdir -p /root/.nvim/
 sudo ln -sf $self_dir/_vimrc /root/.config/nvim/init.vim
 sudo ln -sf $HOME/.nvim/bundle /root/.nvim/bundle
 
-echo ""
-echo ""
-echo "Modify i3status.conf for network!"
