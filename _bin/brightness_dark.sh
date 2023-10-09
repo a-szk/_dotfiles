6924e04a147bd840e2bfead0e334f1d22a2562eb
a-szk <atsushisuzuki2k@gmail.com>
2023-10-09 10:11:09 +0900

    modify brightness_dark.sh

--- _bin/brightness_dark.sh
+++ _bin/brightness_dark.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-xrandr --output eDP-1 --brightness 0.2
+if [ $# -ne 0 ]; then
+  xrandr --output eDP-1 --brightness $1
+  exit 1
+else
+  xrandr --output eDP-1 --brightness 0.2
+  exit 1
+fi
 
 
