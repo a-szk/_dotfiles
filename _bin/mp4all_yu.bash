fa5163da2f3939b4db11b6013f5a90b3f4d95fc8
a-szk <atsushisuzuki2k@gmail.com>
2024-02-03 09:48:02 +0900

    rename mp4conv.bash for all current .mp4

---
+++ _bin/mp4all_yu.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ディレクトリ内のすべての.mp4ファイルをwinでも見られるフォーマットに変換する
+
+if [ $# -ne 0 ]; then
+  echo "argument error:\nusage:\nmp4conv_yu.sh" 1>&2
+  exit 1
+fi
+
+mkdir conv
+ls | grep -E "*.mp4" | xargs -IXXX -n 1 -P32 ffmpeg -i XXX -acodec copy -pix_fmt yuv420p conv/XXX
