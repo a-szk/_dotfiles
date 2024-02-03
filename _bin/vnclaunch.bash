0ff6ed6a57b118720d92a44b0dd66e034918a89a
a-szk <atsushisuzuki2k@gmail.com>
2024-02-03 09:45:21 +0900

    modify vnclaunch

--- _bin/vnclaunch.bash
+++ _bin/vnclaunch.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 killall -9 x11vnc
-x11vnc -display :1 -rfbport 5900 -nopw -auth guess -noxdamage -nomodtweak -clear_all
+x11vnc -display :0 -rfbport 5900 -nopw -auth guess -noxdamage -nomodtweak -clear_all
 
 
